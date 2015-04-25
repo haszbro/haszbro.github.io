a2d0f0f0ff0f975c738dadbbce79dd4e447075c2
haszbro <alex.haszard@gmail.com>
2015-04-26 11:24:01 +1200

    random number and background random fix

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ window.makeNumber = function () {
 	console.log('poop');
 	return Math.random(1,10);
 };
+
+window.randomColour = function () {
+	console.log('poo');
+	document.body.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+};
